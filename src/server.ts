8c1ff3e6ce4b9e10db46419678c216db4799ad53
Gustavo <gustavoc.moraes@hotmail.com>
2021-01-18 14:49:21 -0300

    dependencies installed

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { response } from 'express';
+import routes from './routes'
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Hellow' });
+})
+
+app.listen(3333);
+
+console.log('working');
