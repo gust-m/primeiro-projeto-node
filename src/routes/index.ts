e8a7a9f910bb3cb2095a80a1da9f7a8b81e95877
Gustavo <gustavoc.moraes@hotmail.com>
2021-01-19 23:22:36 -0300

    Post routes created for test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,18 @@
 import { Router } from 'express';
 
+
 const routes = Router();
+
+routes.post('/users', (request, response) => {
+  console.log(request.body);
+
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  }
+  return response.json(user);
+});
 
 export default routes;
