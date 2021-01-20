c9f8c19a972d8844395b7ae322fdc07160da0039
Gustavo <gustavoc.moraes@hotmail.com>
2021-01-19 23:23:54 -0300

    starting route application

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express, { response } from 'express';
+import express, { json } from 'express';
 import routes from './routes'
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hellow' });
-})
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333);
 
-console.log('working');
+app.listen(3333, () => {
+  console.log('working');
+});
