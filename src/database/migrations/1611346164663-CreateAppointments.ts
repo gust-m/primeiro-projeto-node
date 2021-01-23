226787deef26af0ef2ace103e6fe2808db83a8a1
Gustavo <gustavoc.moraes@hotmail.com>
2021-01-23 16:18:36 -0300

    Appointment fields added

--- src/database/migrations/1611346164663-CreateAppointments.ts
+++ src/database/migrations/1611346164663-CreateAppointments.ts
@@ -2,14 +2,14 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateAppointments1611346164663 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
+    public async up(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.createTable(
         new Table({
           name: 'appointments',
           columns: [
             {
               name: 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()',
@@ -17,19 +17,27 @@ export class CreateAppointments1611346164663 implements MigrationInterface {
             {
               name: 'provider',
               type: 'varchar',
-              isNullable: false,
             },
             {
               name: 'date',
               type: 'timestamp with time zone',
-              isNullable: false,
-            }
-          ]
-        })
-      )
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+          ],
+        }),
+      );
     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
+    public async down(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.dropTable('appointments');
     }
 
